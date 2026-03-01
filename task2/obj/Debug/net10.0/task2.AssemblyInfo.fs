f080900305d3a28b8bae9e7bd23df499acc6308a
Михаил Фадеев <evm1t@MacBook-Air-Mihail.local>
2026-03-01 16:19:26 +0500

    fix project and code style

--- task2/obj/Debug/net10.0/task2.AssemblyInfo.fs
+++ task2/obj/Debug/net10.0/task2.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("task1")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("task2")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5a66d756850499081260c6e61ab29e085f2de9")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("task1")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("task1")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("task2")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("task2")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
